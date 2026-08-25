eb51e35becd155598677cbf3610b24f3aacdd063
Aky29 <me.anishkumaryadav@gmail.com>
2026-08-25 17:58:12 +0530

    pydantic and typing:setting up get api

--- tools/backend/fastapi/dev/main.py
+++ tools/backend/fastapi/dev/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
+class iteams(BaseModel):
+    name:str
+    id:int
+    price:float
+    tax:Optional[float]=None
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/items/{name}")
+async def items(name:str, price:int|None =None):
+    return {"item name":name , "price":price}
+
+@app.get("/item")
+async def many_items():
+    items_list = {"name":"itm1","price":100.2,"id":12331,"tax":10.2}
+    return iteams(**items_list)
