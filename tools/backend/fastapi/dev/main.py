1c1272f48587f9036026afcd89ca1dedc1bc77b7
Aky29 <me.anishkumaryadav@gmail.com>
2026-08-29 16:28:26 +0530

    learned: dynamic model

--- tools/backend/fastapi/dev/main.py
+++ tools/backend/fastapi/dev/main.py
@@ -76,3 +76,46 @@ async def read_items(product_id:Annotated[int,Path(ge=1)],db : DBsession):
     if product_id not in db:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail = "Item not present")
     return {'id':product_id,**db[product_id].model_dump()}
+
+
+#dynamic model 
+from typing import Any,Dict,Type,List
+from datetime import date 
+from pydantic import create_model
+
+Category_data = {
+    1:{'name':'Food','fields':{'expiry':(date,...),'ingredients':(List[str],...)}},
+    2:{'name':'Games','fields':{'genre':(List[str],...),'rating':(float,Field(gt=0.0, le=5.0))}},
+    3:{'name':'Electronics','fields':{'specific':(List[str],...)}}
+}
+
+def use_model(id:int)->Type[BaseModel]:
+    cat = Category_data.get(id)
+    if not cat:
+        raise HTTPException(status_code=404,detail=f"Product category {id} not found")
+    base_data = {
+        'sku':(str,...),
+        'price':(float,Field(...,gt=0))
+    }
+    all_data = {**base_data,**cat['fields']}
+    cat_name= cat['name']
+    ProductModel= create_model(
+        f'Dynamic{cat_name}Model',
+        **all_data
+    )
+    return ProductModel
+
+@app.post("/products/ecom/{id}")
+async def creat_dynamic_model(
+    id:int,
+    request_body:Dict[str,Any]
+):
+    Model = use_model(id)
+    try:
+        valid_product = Model(**request_body)
+    except Exception as error:
+        raise HTTPException(status_code=422,detail = str(error))
+    return{
+        'message':"product created",
+        'product': valid_product.model_dump()
+        }
